2f90046dac501153c12f2c82551b578ef6bfe420
matheus.falavigna <matheusfalavigna@gmail.com>
2024-02-26 19:15:08 -0300

    feat: adicionando cors na aplicação

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import swaggerUi from "swagger-ui-express";
 
 import { routes } from "./src/routes";
@@ -8,6 +10,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
